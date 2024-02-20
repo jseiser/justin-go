a96df9b9322a2b33e23531b1a84491557ff366ec
Justin S <justinseiser@gmail.com>
2024-02-20 11:57:51 -0500

    adding context

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/jseiser/justin-go/application"
 )
 
 func main() {
 	app := application.New()
+	fmt.Println("Created new app")
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
+
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
